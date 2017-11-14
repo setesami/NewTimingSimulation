3ba8a3fef4a6c350cf1f79fbf7977685bb657ae1
Dilson de Jesus Damiao <dilson@cern.ch>
2017-11-14 18:18:46 +0100

    add step3 configuration RAW2DIGI_RECO

---
+++ step3_RAW2DIGI_RECO.py
@@ -0,0 +1,81 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('RAW2DIGIRECO', eras.Run2_25ns)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load("Configuration.EventContent.EventContent_cff")
+#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load("CondFormats.CTPPSReadoutObjects.CTPPSPixelDAQMappingESSourceXML_cfi")
+process.load('Configuration.StandardSequences.CTPPSRawToDigi_cff')
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.source = cms.Source("EmptyIOVSource",
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(1),
+    lastValue = cms.uint64(1),
+    interval = cms.uint64(1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:step2_DIGI_DIGI2RAW_100k.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+# Track memory leaks
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",ignoreTotal = cms.untracked.int32(1) )
+
+process.options = cms.untracked.PSet(
+    SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+# Output definition
+
+process.output = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('step3_RAW2DIGI_RECO_ctppsgeo_100k.root'),
+    outputCommands = cms.untracked.vstring("drop *","keep PSimHits*_*_*_*","keep CTPPS*_*_*_*","keep *_RPix*_*_*")
+
+#    outputCommands = process.RECOSIMEventContent.outputCommands+["keep CTPPS_*_*_*","keep PSimHits_*_*_*"]
+)
+
+
+# Additional output definition
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_hlt_relval', '')
+
+#from Geometry.VeryForwardGeometry.geometryRP_cfi import *   
+from Configuration.Geometry.geometry_CTPPS_cfi import XMLIdealGeometryESSource_CTPPS
+process.XMLIdealGeometryESSource = XMLIdealGeometryESSource_CTPPS.clone()
+ 
+from RecoCTPPS.PixelLocal.ctppsPixelLocalReconstruction_cff import *
+process.load("RecoCTPPS.PixelLocal.ctppsPixelLocalReconstruction_cff")
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reco_step = cms.Path(process.ctppsPixelLocalReconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.output_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.reco_step,process.endjob_step,process.output_step)
+
+# filter all path with the production filter sequence
+for path in process.paths:
+    #  getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq
+    getattr(process,path)._seq = getattr(process,path)._seq
+
